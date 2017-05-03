804d09f46fd0dc5cd6baf9d20c9a6cd104964978
Jude Augustine Job <judeaugustinej@gmail.com>
2017-05-03 20:58:59 +0530

    created models for timetracker

--- timetracker/models.py
+++ timetracker/models.py
@@ -1,5 +1,15 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+
+class TimeEntry(models.Model):
+    activity = models.ForeignKey(Activity)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    description = models.TextField(blank=True)
+    start = models.DateTimeField(blank=True, null=True)
+    end = models.DateTimeField(blank=True, null=True)
